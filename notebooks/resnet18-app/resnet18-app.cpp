7aec7c59025876d87d257504c3400bf80794e56f
cedrickchee <cedrick.chee@gmail.com>
2019-05-13 22:45:15 +0800

    Debug ResNet-18 ScriptModule

--- notebooks/resnet18-app/resnet18-app.cpp
+++ notebooks/resnet18-app/resnet18-app.cpp
@@ -10,17 +10,17 @@ int main(int argc, const char* argv[]) {
     }
 
     // Deserialize the ScriptModule from a file using torch::jit::load().
-    std::shared_ptr<torch::jit::script::Module> module = torch::jit::load(argv[1]);
+    //std::shared_ptr<torch::jit::script::Module> module = torch::jit::load(argv[1]);
 
-    assert(module != nullptr);
+    //assert(module != nullptr);
     std::cout << "ok\n";
 
     // Create a vector of inputs.
-    std::vector<torch::jit::IValue> inputs;
-    inputs.push_back(torch::ones({1, 3, 224, 224}));
+    //std::vector<torch::jit::IValue> inputs;
+    //inputs.push_back(torch::ones({1, 3, 224, 224}));
 
     // Execute the model and turn its output into a tensor.
-    at::Tensor output = module->forward(inputs).toTensor();
+    //at::Tensor output = module->forward(inputs).toTensor();
 
-    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+    //std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 }
